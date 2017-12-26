3690b03521a3f18e6c02a6718a6e66fb5d368dee
girmal <girmal5@hotmail.com>
2017-12-26 20:31:02 +0530

    Changes made to Gemfile for heroku

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,62 @@
+class ItemsController < ApplicationController
+
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
+  before_action :own_item, only: [:show, :edit, :update, :destroy]
+
+  def index
+    if user_signed_in?
+    # @items = Item.all.order("created_at DESC")
+      @items = Item.where(:user_id => current_user.id).order("created_at DESC")
+    end
+  end
+
+  def own_item
+    unless current_user == @item.user
+       flash[:alert] = "You cannot Modify this item."
+       redirect_to root_path
+    end
+   end
+
+
+  def new
+    @item = current_user.items.build
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render 'edit'
+    end
+  end
+
+
+def destroy
+  @item.destroy
+  redirect_to root_path
+end
+  def create
+    @item = current_user.items.build(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+private def item_params
+    params.require(:item).permit(:title, :description)
+  end
+
+  def find_item
+    @item = Item.find(params[:id])
+  end
+
+
+end
