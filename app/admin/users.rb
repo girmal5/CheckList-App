1be2dcf3130dd31f87f13128b5f25ba23634b818
girmal <girmal5@hotmail.com>
2017-12-30 17:41:24 +0530

    added users and items feature in admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,4 +1,4 @@
-ActiveAdmin.register Item do
+ActiveAdmin.register User do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
@@ -11,8 +11,5 @@ ActiveAdmin.register Item do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-
-
-
 
 end
